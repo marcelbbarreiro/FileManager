d77921b02d7d2ab4ac1c9fee8abb82c5877c6ff6
marcelbbarreiro <marcelbarreiro@gmail.com>
2022-05-31 09:35:43 +0200

    feat: upload Logo

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
 <div class="Menu">
-<i class="fa-solid fa-folder-magnifying-glass"></i> <h1 class="title">CODE BOX</h1>
-  <h4> File Manager System PHP  </h4></div>
+<i class="fa-solid fa-folder-magnifying-glass"></i> <img src="./icons/Logo_CodeBox.png" alt="Logo" width="300" >
+
+  <h5 class="subtitle"> File Manager System PHP  </h5></div>
